c39951e3707575e29f29cbde3f2590a4b0661abf
Janis Zisenis <janis.zisenis@netlight.com>
2025-07-30 16:55:11 +0200

    Add checksums to homebrew formula

--- cli.rb
+++ cli.rb
@@ -6,14 +6,14 @@ class Cli < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/janisZisenis/mrt-cli/releases/download/v0.1.0/mrt-darwin-arm64"
-      sha256 "" # Add SHA256 checksum here when available
+      sha256 "c16e1abe471a7e7a3547bc16011e606baeb0ded900668a0e9c15bd5572819ebc"
       def install
         bin.install "mrt-darwin-arm64" => "mrt"
       end
     end
     if Hardware::CPU.intel?
       url "https://github.com/janisZisenis/mrt-cli/releases/download/v0.1.0/mrt-darwin-amd64"
-      sha256 "" # Add SHA256 checksum here when available
+      sha256 "00c68c158693adfb4de7b2e1d0b086a95a48366f19e71965dd404b17e1e67561"
       def install
         bin.install "mrt-darwin-amd64" => "mrt"
       end
@@ -23,14 +23,14 @@ class Cli < Formula
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/janisZisenis/mrt-cli/releases/download/v0.1.0/mrt-linux-arm64"
-      sha256 "" # Add SHA256 checksum here when available
+      sha256 "c16e1abe471a7e7a3547bc16011e606baeb0ded900668a0e9c15bd5572819ebc"
       def install
         bin.install "mrt-linux-arm64" => "mrt"
       end
     end
     if Hardware::CPU.intel?
       url "https://github.com/janisZisenis/mrt-cli/releases/download/v0.1.0/mrt-linux-amd64"
-      sha256 "" # Add SHA256 checksum here when available
+      sha256 "00c68c158693adfb4de7b2e1d0b086a95a48366f19e71965dd404b17e1e67561"
       def install
         bin.install "mrt-linux-amd64" => "mrt"
       end
